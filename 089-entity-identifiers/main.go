758aa10899e84f5bc5df405bfd324439e097ac58
George Baptista <george@omame.com>
2020-11-13 17:05:59 +0900

    Example089 Entity Identifiers

---
+++ 089-entity-identifiers/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	guuid "github.com/google/uuid"
+)
+
+type ContactId interface{}
+type PhoneNumber int
+type EmailAddress int
+
+type Contact struct {
+	ContactId
+	PhoneNumber
+	EmailAddress
+}
+
+func genUUID() {
+	id := guuid.New()
+	fmt.Printf("github.com/google/uuid: %s\n", id.String())
+}
+
+func generateContacts() interface{} {
+	var sum []Contact
+	var contact Contact
+
+	for i := 1; i < 5; i++ {
+		contact = Contact{
+			ContactId(guuid.New()),
+			PhoneNumber(i),
+			EmailAddress(i),
+		}
+		sum = append(sum, contact)
+	}
+	return sum
+}
+
+func main() {
+	fmt.Printf("%+v\n", generateContacts())
+}
